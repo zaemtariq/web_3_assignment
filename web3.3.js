49acd575abfbfc9429480ce12fb079aa1360877e
zaemtariq <87516819+zaemtariq@users.noreply.github.com>
2021-10-08 09:15:16 +0500

    Update web3.3.js

--- web3.3.js
+++ web3.3.js
@@ -8,33 +8,6 @@ const privatekey = "d225ffdc07585f0bb743eb515c478869d34a1be7756be415ccec224a13af
 const account1_privatekey = Buffer.from(privatekey, 'hex');
 const account2 = "0x939372C2d0b107f08f714B7156343768d39488E2";
 
-//Async function
-
-/*const fundTranfer = async ()=> {
-    try {
-        const txCount = await web3.eth.getTransactionCount(account1);
-        const txObject = {
-            nonce:      web3.utils.toHex(txCount),
-            to:         account2,
-            value:      web3.utils.toHex(web3.utils.toWei('0.1','ether')),
-            gasLimit:   web3.utils.toHex(21000),
-            gasPrice:   web3.utils.toHex(web3.utils.toWei('10','gwei'))
-    
-        }
-        const tx = new Tx(txObject, { 'chain':'ropsten' });
-        tx.sign(account1_privatekey);
-        const TXSERIALIZED = tx.serialize();
-        const raw = '0x' + TXSERIALIZED.toString('hex');
-        const response = await web3.eth.sendSignedTransaction(raw);
-        console.log(response);
-    }catch(err) {
-        console.log(err);
-    }
-}
-
-fundTranfer();*/
-
-
 web3.eth.getTransactionCount(account1, (error,txCount)=>{
     console.log("nounce value :", txCount);
     const txObject = {
